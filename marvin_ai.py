0000f55596d645c8c109d8da6046d35aec6f338d
Iddo Kerkhof <email@iddo.eu>
2024-01-27 16:33:29 +0100

    The prompt for narrative extraction is improved. gpt-4-1106-preview is used instead of 3.5, because the latter is not good enough

--- marvin_ai.py
+++ marvin_ai.py
@@ -3,7 +3,7 @@ import marvin
 from utils import read_from_file
 
 
-marvin.settings.openai.chat.completions.model = 'gpt-3.5-turbo-16k'
+marvin.settings.openai.chat.completions.model = 'gpt-4-1106-preview'
 
 
 def check_settings():
@@ -12,29 +12,21 @@ def check_settings():
 
 @marvin.fn
 def extract_narratives(transcript: str) -> list[str]:
-    """### CONTEXT
-The text below is a YouTube transcript:
+    """
+### CONTEXT
+The text below is a YouTube transcript  about the Israel-Hamas conflict that started on 7 October 2023:
 ---
 `transcript`
 ---
 
-### INSTRUCTIONS
-Analyze the text above and divide it into distinct narratives. Provide a brief summary for each narrative using active, simple language suitable for subject-verb-object (SVO) structure. Avoid passive language, self-referential phrases, or attributions. Do NOT use phrases like 'is explored', 'is detailed', 'are recounted', 'the narrative describes', or 'according to the speaker'. Present each summary using active verbs and clear subjects, allowing easy identification of the main actors (subjects) and their actions (verbs).
+### OBJECTIVE
+Your objective is to extract and summarize the narratives in this text. Extract at most 10 narratives. If there are more, extract the ones most likely to contain disinformation or hate speech.
 
-### CORRECT EXAMPLES
-- "Herzl's vision of a Jewish state responds to rising anti-Semitism in Europe."
-- "Jewish people maintain a historical presence in Israel for 3,500 years, enduring various conquests."
-- "The Arab Conquest in the 7th century displaces Jewish people, leading to the loss of Jewish sovereignty."
+### SPECIFICS
+When summarizing narratives, avoid using self-referential phrases or attributions like "claims the speaker", "according to the speaker", or any similar terms. Ensure that all statements are in active language and present tense. This is crucial for the accurate creation of triples in a knowledge graph. Avoid using past participles as they can lead to inaccuracies and inconsistencies in the data structure. The results will used to detect hate speech and disinformation in a knowledge graph. If you include passive language or self-referential phrases this will fail  and the disinformation might not be detected.
 
-### WRONG EXAMPLES
-- "The rise of anti-Semitism in Europe and its relation to Herzl's vision of a Jewish state is explored."
-- "The historical presence of Jewish people in the land of Israel is detailed, stretching back 3,500 years."
-- "The Arab Conquest in the 7th century is described as a turning point, resulting in Jewish displacement."
-- "The narrative describes the impact of Herzl's vision on European anti-Semitism."
-- "According to the speaker, Jewish people have been in Israel for over 3,500 years."
-
-### OUTPUT FORMAT
-Respond as a list of strings, one for each narrative.
+### RESULT
+Respond as a list of strings, one for each narrative in the format ["string1",  "string2"].
 """
 
 @marvin.fn
